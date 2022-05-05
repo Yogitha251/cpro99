959dceada258da504ac630ce00ca27ab5615de22
Yogitha251 <74713251+Yogitha251@users.noreply.github.com>
2022-05-05 12:09:09 +0530

    Add files via upload

--- backupfiles.py
+++ backupfiles.py
@@ -3,33 +3,84 @@ import shutil
 import stat
 import time
 
-path='C:\Users\veena\OneDrive\Pictures\Saved Pictures'
+def main():
 
-days=3
-seconds=time.time()-(days*24*60*60)
+	deletedfolderscount = 0
+	deletedfilescount = 0
+
+	path = "C:\Users\veena\OneDrive\Pictures\Saved Pictures"
+
+	days = 30
+
+	seconds = time.time()-(days * 24 * 60 * 60)
+
+	if os.path.exists(path):
+		for root_folder, folders, files in os.walk(path):    
+			if seconds >= get_file_or_folder_age(root_folder):
+
+				remove_folder(root_folder)
+				deletedfolderscount += 1 
+
+				break
+
+			else:
+				for folder in folders:
+					folder_path = os.path.join(root_folder, folder)
+
+					if seconds >= get_file_or_folder_age(folder_path):
+						remove_folder(folder_path)
+						deletedfolderscount += 1 
+
+				for file in files:
+					file_path = os.path.join(root_folder, file)
+					if seconds >= get_file_or_folder_age(file_path):
+
+						remove_file(file_path)
+						deletedfilescount += 1 
+
+		else:
+
+			if seconds >= get_file_or_folder_age(path):
+
+				remove_file(path)
+				deletedfilescount += 1 
+
+	else:
+
+		print(f'"{path}" is not found')
+		deletedfilescount += 1
+
+	print(f"Total folders deleted: {deletedfolderscount}")
+	print(f"Total files deleted: {deletedfilescount}")
 
 
-list=os.listdir(path)
-print(list)
+def remove_folder(path):
 
-for file in list:
-    name,ext=os.path.splitext(file)
-    #print(name)
-    #print(ext)
-    
-    ext=ext[1:]
-    
-    #print(ext[1:2])
+	if not shutil.rmtree(path):
 
-    
-    if ext=='':
-        continue
+		print(f"{path} is removed successfully")
 
-    if os.path.exists(path+'/'+ext):
-      # shutil.move(path+'/'+file,path+'/'+ext+'/'+file)
-      for folders,files in os.walk(path):
-          
+	else:
 
-    else:
-        os.makedirs(path+'/'+ext)
-        shutil.move(path+'/'+file,path+'/'+ext+'/'+file)
+		print(f"Unable to delete the "+path)
+
+def remove_file(path):
+
+	if not os.remove(path):
+
+		print(f"{path} is removed successfully")
+
+	else:
+
+		print("Unable to delete the "+path)
+
+
+def get_file_or_folder_age(path):
+
+	ctime = os.stat(path).st_ctime
+
+	return ctime
+
+
+if __name__ == '__main__':
+	main()
